d0b5b1b972ab56eecc3dcc8ab51623fa251e3c2d
AhmedReda958 <areda.gad@gmail.com>
2024-06-18 20:13:45 +0300

    main script

---
+++ README.md
@@ -0,0 +1,38 @@
+# Bcrypt Password Comparison Script
+
+This is a Node.js script that compares a hashed password with a list of hashed passwords from a file named `passwords.txt`. The hashing algorithm used is bcrypt.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+What things you need to install the software and how to install them:
+
+- Node.js
+- npm
+
+### Installing
+
+A step by step series of examples that tell you how to get a development environment running:
+
+1. Clone the repository: `git clone https://github.com/yourusername/yourrepository.git`
+2. Navigate into the directory: `cd yourrepository`
+3. Install the dependencies: `npm install`
+
+### Usage
+
+To run the script, use the following command:
+
+```bash
+npm start
+```
+
+or
+
+```bash
+node index.js
+```
+
+Make sure to create a `passwords.txt` file with your passwords, with each password on a separate line. Then, run the project.
---
+++ index.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const bcrypt = require("bcrypt");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// compare hashed password function
+async function comparePasswords(hashedPassword, passwordsList) {
+  // loop through the passwords
+  for (let i = 0; i < passwordsList.length; i++) {
+    const password = passwordsList[i];
+    // compare the password with the hashed password
+    await bcrypt.compare(password, hashedPassword, (err, res) => {
+      if (err) {
+        console.log("Error comparing the passwords");
+        console.error(err);
+        return;
+      }
+      // if the password is found
+      if (res) {
+        console.log("Password found: " + password);
+        rl.close();
+        return password;
+      }
+      if (i === passwordsList.length - 1 && !res) {
+        console.log("Password not found");
+        rl.close();
+        return null;
+      }
+    });
+
+    console.clear();
+    console.log(
+      "Passwords checked: " + (i + 1),
+      "(" + Math.floor(((i + 1) / passwordsList.length) * 100) + "%)"
+    );
+  }
+}
+
+// read the password.txt file
+fs.readFile("passwords.txt", "utf8", async (err, data) => {
+  if (err) {
+    console.log("Error reading the file");
+    console.error(err);
+    return;
+  }
+  // split the data by new line
+  const passwordsList = data.split("\r\n");
+  // get the password from the user
+  rl.question("Enter the hashed password: ", async (hashedPassword) => {
+    // compare the password
+    await comparePasswords(hashedPassword, passwordsList);
+  });
+});
+
+rl.on("close", () => {
+  console.log("Exiting...");
+  process.exit(0);
+});
